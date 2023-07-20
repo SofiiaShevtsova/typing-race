60464ea3019a341cf53655851b780c9fa097c7af
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-07-20 17:48:00 +0300

    + text for game

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,11 @@
 export const texts = [
-  "Text for typing #1",
-  "Text for typing #2",
-  "Text for typing #3",
-  "Text for typing #4",
-  "Text for typing #5",
-  "Text for typing #6",
-  "Text for typing #7",
+  "Good day! My name is Sofia, and I am a future software developer. That is why I am currently actively learning JavaScript. I really enjoyed the quest created by the developers from BS, so I decided to enroll and study at BSA. The selection tasks were also really cool. Naturally, I expect to enhance my knowledge and acquire new skills through my studies. My ultimate goal is to secure a job as a developer in the future. I dedicate a significant amount of time to studying since I am just at the beginning of my journey. I am confident in my ability to succeed and in the path I have chosen.",
+  "If the motorcycle was huge, it was nothing to the man sitting astride it. He was almost twice as tall as a normal man and at least five times as wide. He looked simply too big to be allowed, and so wild - long tangles of bushy black hair and beard hid most of his face, he had hands the size of trash can lids, and his feet in their leather boots were like baby dolphins. In his vast, muscular arms he was holding a bundle of blankets.",
+  "Holmes is interesting because his personality has many contradictions. He is  methodical in his work but untidy in his private life: he keeps his tobacco in the end of a slipper and rarely answers his letters. He is a man of science but also a heavy smoker. He is hard working when he is on a case, but when he has nothing to do, he is bored and lazy. He is an expert in certain fields related to his detective work, but completely ignorant about many other things. He once claimed that he didn’t know the Earth revolved around the Sun because it wasn’t relevant to his work. His interests are mostly intellectual, but he also plays the violin and is an expert at boxing and sword fighting.",
+  "But that did not really surprise me much. I knew very well that in addition to the great planets− such as the Earth, Jupiter, Mars, Venus− to which we have given names, there are also hundreds of others, some of which are so small that one has a hard time seeing them through the telescope. When an astronomer discovers one of these he does not give it a name, but only a number. He might call it, for example, 'Asteroid 325.'",
+  "Dany could not have said why the city needed a gate when it had no walls . . . and no buildings that she could see. Yet there it stood, immense and beautiful, the great horses framing the distant purple mountain beyond. The bronze stallions threw long shadows across the waving grasses as Khal Drogo led the khalasar under their hooves and down the godsway, his bloodriders beside him.",
+  "They all leaned forward straining at the paddles: even Sam took a hand. Every moment they expected to feel the bite of black-feathered arrows. Many whined overhead or struck the water nearby; but there were no more hits. It was dark, but not too dark for the night-eyes of Orcs, and in the star-glimmer they must have offered their cunning foes some mark, unless it was that the grey cloaks of Lo´rien and the grey timber of the elf-wrought boats defeated the malice of the archers of Mordor.",
+  "Four years later (2758) great troubles came to Rohan, and no help could be sent from Gondor, for three fleets of the Corsairs attacked it and there was war on all its coasts. At the same time Rohan was again invaded from the East, and the Dunlendings seeing their chance came over the Isen and down from Isengard. It was soon known that Wulf was their leader. They were in great force, for they were joined by enemies of Gondor that landed in the mouths of Lefnui and Isen.",
 ];
 
 export default { texts };
